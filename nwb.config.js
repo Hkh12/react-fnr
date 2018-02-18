3bbb5a983bd1441f63bfdedfaa627e9328444027
Hosein Khansari <hrk.x11@gmail.com>
2018-02-18 20:25:59 +0330

    🔧 change config

--- nwb.config.js
+++ nwb.config.js
@@ -1,12 +1,18 @@
 module.exports = {
-  type: 'react-component',
-  npm: {
-    esModules: false,
-    umd: {
-      global: 'ReactFNR',
-      externals: {
-        react: 'React'
-      }
+    type: 'react-component',
+    npm: {
+        esModules: false,
+        umd: {
+            global: 'ReactFNR',
+            externals: {
+                react: 'React'
+            }
+        }
+    },
+    webpack: {
+        html: {
+            mountId: 'root',
+            title: 'react-fnr test'
+        }
     }
-  }
 }
