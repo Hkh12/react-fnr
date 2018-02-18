5ec78b2458a5ab3708234964107f9acd62324678
Hosein Khansari <hrk.x11@gmail.com>
2018-02-18 20:39:21 +0330

    🚨 initial test

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,14 +1,26 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, {Component} from 'react';
+import {render} from 'react-dom';
 
-import Example from '../../src'
+import FNR from '../../src';
 
 class Demo extends Component {
   render() {
     return <div>
-      <Example/>
+      <FNR url='https://api.piorra.ir/wikipedia?key=Test&value=React_(JavaScript_library)' component={
+              (data, url) => {
+                  return (<div>
+                      <h1>Fetched from: <span style={{color: '#673ab7'}}>{url}</span></h1>
+                      <hr />
+                      <h1>{data.result.title}</h1>
+                      <p>
+                          {data.result.extract}
+                      </p>
+                      <br/>
+                  </div>)
+              }
+          } />
     </div>
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo/>, document.querySelector('#root'))
